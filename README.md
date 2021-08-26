ff476ae4c0e5b9086b512ebc9076150fd7e164bf
dzsak <sisakadam@gmail.com>
2021-08-26 08:52:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-7ajs9w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-7ajs9w)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+import Basic from './Basic';
+export default function App() {
+  return (
+    <div>
+      <Basic />
+    </div>
+  );
+}
